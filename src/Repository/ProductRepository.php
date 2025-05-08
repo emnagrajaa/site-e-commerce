8f4f48bfb540ce07c60b25ce4b7012d7441f8487
Minyar Frifita <minyar.frifita@insat.ucar.tn>
2025-05-08 23:11:54 +0100

    Ajout du formulaire ProductType et personnalisation de ProductRepository - Minyar

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -16,28 +16,36 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
-    //    /**
-    //     * @return Product[] Returns an array of Product objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    public function save(Product $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
 
-    //    public function findOneBySomeField($value): ?Product
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function remove(Product $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function findBySearch(string $query): array
+    {
+        return $this->createQueryBuilder('p')
+            ->where('p.name LIKE :query OR p.description LIKE :query')
+            ->setParameter('query', '%' . $query . '%')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findAvailable(): array
+    {
+        return $this->createQueryBuilder('p')
+            ->where('p.stock > 0')
+            ->getQuery()
+            ->getResult();
+    }
 }
